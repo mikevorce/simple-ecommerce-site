70e7f8f3810f93c2e53cf61b444af0784d5c5955
Mike Vorce <michael.j.vorce@gmail.com>
2019-05-25 18:32:08 -0500

    Add component templates and routes for site pages

---
+++ src/components/Default.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Default extends Component {
+	render() {
+		return (
+			<div>
+				<h3>404 Oh Nos!... Page Not Found</h3>
+			</div>
+		);
+	}
+}
