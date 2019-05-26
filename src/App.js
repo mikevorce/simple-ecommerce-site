c730523aa690c7614041cacde436d8d29de67150
Mike Vorce <michael.j.vorce@gmail.com>
2019-05-26 16:06:02 -0500

    Add 'styled-components' library as dependency

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 import Navbar from './components/Navbar'
 import ProductList from './components/ProductList'
 import Details from './components/Details'
 import Cart from './components/Cart'
 import Default from './components/Default'
+
+import logo from './logo.svg';
 
 
 function App() {
